e7ae69606c8a84631e3a1e289521e20f127a962c
Stefan Stoychev <stefan.stoychev44@gmail.com>
2024-01-15 16:59:10 +0200

    Change defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command, mode }) => {
-    return {
-        resolve: {
-            alias: {
-                'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
-            }
-        }
-    };
-},);
+export default defineConfig({
+    base: "/GameJam2024/",
+});
